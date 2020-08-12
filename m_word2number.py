017c2610edae7c60182ef4b89bc5876058fb9bd7
FahimArman <fahim.3761@gmail.com>
2020-08-13 00:43:34 +1000

    word2number added

---
+++ m_word2number.py
@@ -0,0 +1,13 @@
+from word2number import w2n
+
+print(w2n.word_to_num('two million three thousand nine hundred and eighty four'))
+print(w2n.word_to_num('two point three'))
+print(w2n.word_to_num('112'))
+print(w2n.word_to_num('point one'))
+print(w2n.word_to_num('one hundred thirty-five'))
+
+
+
+
+
+
